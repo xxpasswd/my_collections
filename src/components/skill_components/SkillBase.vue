e158fc7a112a8b3f51f98cf3eb48457eb59670ed
xxpassword <1490621113@qq.com>
2018-11-01 14:08:07 +0800

    add python book

--- src/components/skill_components/SkillBase.vue
+++ src/components/skill_components/SkillBase.vue
@@ -5,6 +5,7 @@
         <li><a>《python核心编程》</a></li>
         <li><a href="http://python3-cookbook.readthedocs.io/zh_CN/latest/index.html">《python cookbook》</a></li>
         <li><a>《fluent python》</a></li>
+        <li><a>《python 3面向对象编程》</a></li>
     </ul>
      <h2>算法与数据结构</h2>
      <ul>
@@ -20,8 +21,10 @@
          <li><a href="https://linuxtools-rst.readthedocs.io/zh_CN/latest/">linux工具快速教程</a></li>
      </ul>
      <h2>数据库</h2>
+     <ul>
         <li><a>《mysql必知必会》</a></li>
         <li><a>《SQL学习指南》</a></li>
+     </ul>
      <h2>版本控制</h2>
      <ul>
          <li><a href="https://git-scm.com/book/en/v2">《Progit》</a></li>
